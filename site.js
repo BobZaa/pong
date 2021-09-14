f72e4b79cee7b6354d8a5d933cea8d6cd6be003b
BobZaa <55461085+BobZaa@users.noreply.github.com>
2021-09-14 20:41:40 +0200

    update

---
+++ site.js
@@ -0,0 +1,8 @@
+window.onload; choosePic
+
+    var myPictures = new Array("images/reklam.png")("images/PEHOAGPS_BEST_AD_EUNE.png"); 
+
+function choosePic(){
+    var randomNum = Math.floor(Math.random() * myPictures.length);
+}
+    
