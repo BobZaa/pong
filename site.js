e0033189edef2a98e5c5faf74787b3cff2f11797
BobZaa <bobzaagameplay@gmail.com>
2021-09-27 14:48:04 +0200

    fixed anonymous

--- site.js
+++ site.js
@@ -1,16 +1,11 @@
-window.onload = choosePics;
-
-const myPictures = ["reklam.png", "PEHOAGPS_BEST_AD_EUNE.png", "BETTER_AD.jpg"]; 
-    
-function choosePics(){
+const myPictures = ["reklam.png", "PEHOAGPS_BEST_AD_EUNE.png", "BETTER_AD.jpg"];
+                                                 
+window.onload = a => {
     let randomNum = Math.floor(Math.random() * myPictures.length);
-
     const malware = document.querySelector(".malware");    
     malware.src = myPictures[randomNum];
-
+                                                                  
     randomNum = Math.floor(Math.random() * myPictures.length);
     const wadbot = document.querySelector(".wadbot");    
     wadbot.src = myPictures[randomNum];
 }
-    
-    
